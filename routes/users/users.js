c766986626567416d57271cb47bba30268069f71
Halyna Sinko <galinasinko14@gmail.com>
2021-10-25 17:35:45 +0300

    Update users.js

--- routes/users/users.js
+++ routes/users/users.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const {
+  validateSignupUser,
+  validateLoginUser,
+  validateUpdateSubscription,
+} = require("./validation");
+const {
   signup,
   login,
   logout,
@@ -9,15 +14,10 @@ const {
   updateUserSubscription,
 } = require("../../controllers/users.controller");
 const guard = require("../../helpers/guard");
-
-const {
-  validateSignupUser,
-  validateLoginUser,
-  validateUpdateSubscription,
-} = require("./validation");
+const loginLimiter = require("../../helpers/rate-limit");
 
 router.post("/signup", validateSignupUser, signup);
-router.post("/login", validateLoginUser, login);
+router.post("/login", validateLoginUser, loginLimiter, login);
 router.post("/logout", guard, logout);
 router.get("/current", guard, getCurrentUser);
 router.patch("/", guard, validateUpdateSubscription, updateUserSubscription);
